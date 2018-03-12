7a49cd7d58a0d6bb7b4dbe91b90ce0524a55ae03
Zac Espinosa <zespinos@stanford.edu>
2018-03-11 19:10:46 -0700

    updates

--- image_classifier.py
+++ image_classifier.py
@@ -1,26 +1,37 @@
 import os
 from subprocess import Popen, call
 import sys
+import pdb
+
+def correctPath():
+    path = os.getcwd()
+    pathlist = path.split('\\')
+    path = 'C:'
+    for index in range(1,len(pathlist)):
+        #if pathlist[index] == "Stanford Courses": path = path + '\\' + '"Stanford Courses"'
+        path = path + '\\' + pathlist[index]
+    return path
 
 def classify_images():
-  for filename in os.listdir('unlabeled'):
-    viewer = Popen(['open', 'unlabeled/' + filename])
+  path = correctPath()
+  path = path + '\\unlabeled\\'
+  for filename in os.listdir(path):
+    print(path + filename)
+    viewer = Popen(['open', path  + filename])
     # Get amount of floating vegetation
     floating = ''.join(input('Amount of floating vegetation? (1-9)\n').split())
     while not floating.isdigit():
       print('Sorry, invalid response. Please try again.')
       floating = ''.join(input('Amount of floating vegetation? (1-9)\n').split())
-    
+
     # Get amount of emergent vegetation
     emergent = ''.join(input('Amount of emergent vegetation? (1-9)\n').split())
     while not emergent.isdigit():
       print('Sorry, invalid response. Please try again.')
       emergent = ''.join(input('Amount of emergent vegetation? (1-9)\n').split())
-  
+
     copy_command = 'cp unlabeled/' + filename + ' labeled/' + floating + '-' + emergent + '-' + filename
     call(copy_command.split())
-
-
 
 def main():
   print('Welcome to the vegetation classifier!')
@@ -32,4 +43,3 @@ def main():
 
 if __name__ == '__main__':
   main()
-
