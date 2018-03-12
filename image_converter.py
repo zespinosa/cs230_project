ae2469c0c00b8d70be7855ebabf1b9fecdff188d
Zac Espinosa <zespinos@stanford.edu>
2018-03-11 19:12:14 -0700

    updates

--- image_converter.py
+++ image_converter.py
@@ -1,32 +1,8 @@
-# import os
 from PIL import Image
-
-# # Converts .tifs to .png for training set
-
-# current_path = os.getcwd() 
-# for full_filename in os.listdir('parsed_tifs/'):
-#   filename, ext = os.path.splitext(full_filename)
-#   if ext == '.tif':
-#     if os.path.isfile('unlabeled/' + filename + '.png'):
-#       print('A .png file already exists for ' + filename)
-#     # If a png with the name does *NOT* exist, covert one from the tif.
-#     else:
-#       outfile = 'unlabeled/' + filename + '.png'
-#       try:
-#         im = Image.open('parsed_tifs/' + full_filename)
-#         print('Converting jpeg for ' + filename)
-#         out = im.convert('RGB')
-#         out.save(outfile, 'PNG', quality=100)
-#       except Exception as e:
-#         print(e)
-        
-
-
 import numpy as np
 import rasterio
 
 # Read raster bands directly to Numpy arrays.
-#
 with rasterio.open('./parsed_tifs/Parsed_TIFF.99.tif') as src:
   r, g, b, a = src.read()
   meta = src.meta
@@ -59,7 +35,6 @@ with rasterio.open('example-total.tif', 'w', **meta) as dst:
   #   dest = np.zeros_like(band)
   #   dst.write(dest.astype(rasterio.uint8), indexes=i)
 
-  
 outfile = 'translated-tif.jpg'
 try:
   im = Image.open('example-total.tif')
