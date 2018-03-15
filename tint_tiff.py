b71bcc2bcdd418e3a273a3de3cff47cb12ef3b7b
michaelvobejda <mvobejda@stanford.edu>
2018-03-14 23:17:21 -0700

    Tweak tinting

--- tint_tiff.py
+++ tint_tiff.py
@@ -11,16 +11,22 @@ def tint_tiff(X, g_delta, b_delta):
       r, g, b, a = tif.read()
       meta = tif.meta
 
-    with rasterio.open(out_directory + filename + '-both.tif', 'w', **meta) as dst:
+    with rasterio.open(out_directory + filename + '-tint.tif', 'w', **meta) as dst:
       if g_delta == 1 and b_delta == 1:
         return
 
-      r //= 10
-      g //= 10
-      b //= 10
+      r //= 9
+      g //= 9
+      b //= 9
 
+      r *= (9 - g_delta)
+      r *= (9 - b_delta)
       g *= g_delta
       b *= b_delta
+
+      # r *= (9 - b_delta)
+      # g *= (9 - b_delta) 
+      # b *= b_delta
 
       dst.write_band(1, r)
       dst.write_band(2, g)
@@ -45,8 +51,16 @@ def tint_tiff(X, g_delta, b_delta):
       # })
       # print(dst.profile)
 
+test_X = [
+  '1-1-unlabeled.tif.123',
+  '1-1-unlabeled.tif.125',
+  '1-1-unlabeled.tif.761',
+  '3-3-unlabeled.tif.1280',
+  '9-3-unlabeled.tif.549',
+]
 
-tint_tiff(test_X, 9, 9)
+
+tint_tiff(test_X, 4, 1)
 
 
   
